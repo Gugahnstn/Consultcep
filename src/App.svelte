1330abc327eca8ecec8ffb12856af36713d995f7
Gugahnstn <gustavonstn@hotmail.com>
2022-07-12 12:22:25 -0300

    [refactor]: initial commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<div class="em-breve">
+  <p>Consultcep - Em breve refatorado</p>  
+</div>
+
+<style>
+  .em-breve {
+    margin-top: 15%;
+  }
+  .em-breve p {
+    text-align: center;
+    font-size: 40px;
+  }
+</style>
