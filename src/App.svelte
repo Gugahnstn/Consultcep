759fd6a5942b6a15dc10772bca9e71279be0dee7
Gugahnstn <gustavonstn@hotmail.com>
2022-10-10 00:32:04 -0300

    feat: adicionando componentes e estilização.

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,32 @@
-<div class="em-breve">
-  <p>Consultcep - Em breve refatorado</p>  
+<script>
+  import Header from "./lib/Header.svelte";
+  import Hero from "./lib/Hero.svelte";
+  import Main from "./lib/Main.svelte";
+</script>
+
+<div class="top_Line"><p>Para mais informações acesse o <a href="https://github.com/gugahnstn/consultcep">repositório</a></p></div>
+<div class="container">
+  <Header/>
+  <Hero/>
+  <Main/>
 </div>
 
 <style>
-  .em-breve {
-    margin-top: 15%;
+  .container {
+    width: 1200px;
+    margin: 0 auto;
   }
-  .em-breve p {
+
+  .top_Line {
+    background: radial-gradient(180.24% 202137.42% at 21.04% 48.84%, #3F3D56 0%, #55FF99 100%);
+    font-family: var(--font-inter);
+    padding: 0.5rem 2rem;
     text-align: center;
-    font-size: 40px;
+    color: #ffff;
+  }
+
+  .top_Line p a {
+    font-weight: 700;
+    color: #ffff;
   }
 </style>
