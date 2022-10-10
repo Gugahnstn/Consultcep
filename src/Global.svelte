153051aa67db1674f0026a3186af254a9dc1a58e
Gugahnstn <gustavonstn@hotmail.com>
2022-10-10 00:32:23 -0300

    feat: adicionando estilizaçòes globais.

---
+++ src/Global.svelte
@@ -0,0 +1,19 @@
+<style>
+  :global(*) {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :global(body) {
+    background: var(--background-color);
+    font-family: sans-serif;
+  }
+
+  :global(:root) {
+    --font-inter: 'Inter', sans-serif;
+    --font-poppins: 'Poppins', sans-serif;
+    --background-color: #1E202B;
+    --primary-color: #55FF99;
+  }
+</style>
